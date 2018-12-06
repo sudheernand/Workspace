c12152b324cef5791684a7b0f67dc1e8f947b893
cnet <cnet@DESKTOP-3RFGMUL>
2018-12-06 11:29:58 -0600

    Project For jenkins test

---
+++ JenkinsPipelineTest/src/main/java/com/cnet/JenkinsTest.java
@@ -0,0 +1,8 @@
+package com.cnet;
+
+public class JenkinsTest {
+public void jenkinsMeth() {
+	System.out.println("For Jenkins pipeline");
+
+}
+}
---
+++ JenkinsPipelineTest/src/test/java/com/cnet/JenkinsTestCase.java
@@ -0,0 +1,15 @@
+package com.cnet;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class JenkinsTestCase {
+
+	@Test
+	public void testJenkinsMeth() {
+		assertFalse(false);
+
+	}
+
+}
